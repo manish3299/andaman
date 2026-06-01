d2c2669bb8062de67070c397f2616128e806e961
Manish Ambarwar <97036992+manish3299@users.noreply.github.com>
2026-06-01 05:01:09 +0000

    fix vercel build and dependancy

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,12 +1,4 @@
 import type { NextConfig } from 'next';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
